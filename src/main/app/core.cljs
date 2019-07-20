c480a18b724cb8dfdd2621730fd8c75e8ed70bdf
jigkoxsee <supakorn@jigko.net>
2019-07-20 16:50:36 +0700

    add balance and wallet/txs subs

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -17,6 +17,27 @@
      :mempool []
      :blocks [(t/Block. 0 (js/Date.) [(t/Tx. :gen0 nil :vitalik 50 nil 0 0)])]}))
 
+
+(defn mempool-panel []
+  [:div
+    [:h2 "mempool"]
+    (let [txs @(rf/subscribe [:mempool])]
+      (json-html.core/edn->hiccup txs))])
+
+(defn wallet-txs [wallet]
+  (let [txs @(rf/subscribe [:wallet/txs wallet])]
+    [:div
+     [:h3 (str "wallet: " wallet)]
+     (json-html.core/edn->hiccup txs)]))
+
+
+(defn experiment-panel []
+  [:div
+   [:h2 "Experiment"]
+   [wallet-txs :ziko]
+   ;[wallet-txs :vitalik]
+   [mempool-panel]])
+
 (defn main-panel []
   [:div.container
    [:h2 "Blockchain viz"]
@@ -29,9 +50,7 @@
           (json-html.core/edn->hiccup txs))]]
       [:div.row
        [:div.col-md
-        [:h2 "mempool"]
-        (let [txs @(rf/subscribe [:mempool])]
-          (json-html.core/edn->hiccup txs))]]]
+        [experiment-panel]]]]
      [:div.col-md
       [:h2 "blocks"]
       (let [blocks @(rf/subscribe [:blocks-sorted])]
@@ -40,14 +59,17 @@
 (comment
   (println "heyyy")
   (rf/dispatch [:creators/add "ziko"])
-  (rf/dispatch [:tx/new (t/Tx. :id :vitalik :ziko :30 nil 0 1)])
+
+  (rf/dispatch [:tx/new (t/Tx. :mem1 :vitalik :ziko :30 nil 1 1)])
+  (rf/dispatch [:tx/new (t/Tx. :mem2 :vitalik :ziko :30 nil 2 2)])
+  (rf/dispatch [:tx/new (t/Tx. :mem3 :ziko :satoshi :5 nil 1 3)])
 
   (rf/dispatch-sync [:initialize])
 
-  (rf/dispatch [:blocks/add (t/Block. 1 (js/Date.) [(t/Tx. :gen0 nil      :vitalik 50 nil 0 0)])])
-  (rf/dispatch [:blocks/add (t/Block. 2 (js/Date.) [(t/Tx. :gen1 :vitalik :ziko 10 nil 0 1)])])
-  (rf/dispatch [:blocks/add (t/Block. 3 (js/Date.) [(t/Tx. :gen2 :vitalik :ziko 20 nil 0 2)
-                                                    (t/Tx. :gen3 :ziko :satoshi 5 "donate" 0 1)])])
+  (rf/dispatch [:blocks/add (t/Block. 1 (js/Date.) [(t/Tx. :gen1 nil      :vitalik 50 nil 0 0)])])
+  (rf/dispatch [:blocks/add (t/Block. 2 (js/Date.) [(t/Tx. :gen2 :vitalik :ziko 10 nil 0 1)])])
+  (rf/dispatch [:blocks/add (t/Block. 3 (js/Date.) [(t/Tx. :gen3 :vitalik :ziko 20 nil 1 2)
+                                                    (t/Tx. :gen4 :ziko :satoshi 5 "donate" 0 1)])])
 
   (json-html.core/edn->hiccup {:foo [1 2 3] :bar "baz"})
   ;(json-html.core/edn->html {:foo [1 2 3] :bar "baz"})
--- src/main/app/sub.cljs
+++ src/main/app/sub.cljs
@@ -39,23 +39,52 @@
           (remove (comp nil? first keys))
           (apply merge-with +)))))
 
+
+(rf/reg-sub
+  :balance
+  :<- [:world-state]
+  (fn [state [_ wallet]]
+    (get state wallet)))
+
+
+(rf/reg-sub
+  :wallet/txs
+  :<- [:blocks]
+  (fn [blocks [_ wallet]]
+    (let [txs (for [{:keys [timestamp txs :as b]} blocks
+                    {:keys [id from to :as tx]} txs]
+                {from [{:type :out :id id :time timestamp}]
+                 to [{:type :in :id id :time timestamp}]})
+      ;(cljs.pprint/pprint txs)
+          wallets (->> txs
+                       (apply merge-with into)
+                       (remove (comp nil? first))
+                       (into {}))]
+       (get wallets wallet))))
+
 (comment
   (keys {:a 1})
+
+  (rf/subscribe [:balance :vitalik])
+  (rf/subscribe [:balance :fee])
+
+  (rf/subscribe [:wallet/txs :satoshi])
+  (rf/subscribe [:wallet/txs :ziko])
+
+  (merge-with +  {:a 1 :b 2} {:b 1})
+  ;; sub :user/txs
   (let [blocks @(rf/subscribe [:blocks])
-        ops (for [{:keys [no txs :as b]} blocks
-                  {:keys [from to amount fee :as tx]} txs]
-               [
-                {from (- amount)}
-                {from (- fee)}
-                {to amount}])]
-    (->> ops
-      (mapcat identity)
-      (remove (comp nil? first keys))
-      (apply merge-with +))))
+        txs (for [{:keys [timestamp txs :as b]} blocks
+                  {:keys [id from to :as tx]} txs]
+              {from [{:type :out :id id :time timestamp}]
+               to [{:type :in :id id :time timestamp}]})]
+    ;(cljs.pprint/pprint txs)
+    (->> txs
+         (apply merge-with into)
+         (remove (comp nil? first))
+         (into {}))))
 
 
 
-
-       ;[{:wallet from :amount (neg amount)}
-       ; {:wallet to   :amount amount}])))
-
+      ;[{:wallet from :amount (neg amount)}
+      ; {:wallet to   :amount amount}])))
